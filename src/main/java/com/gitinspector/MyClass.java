3d25fb657362330b5668c0abee32a4d175de9721
bcorbett <bcorbett@homeaway.com>
2015-06-11 15:33:16 -0500

    initial commit for project

---
+++ src/main/java/com/gitinspector/MyClass.java
@@ -0,0 +1,13 @@
+/* Copyright Homeaway, Inc 2005-2008. All Rights Reserved.
+ * No unauthorized use of this software.
+ */
+package com.gitinspector;
+
+/**
+ * bcorbett has not yet bothered to enter a useful javadoc comment.
+ */
+public class MyClass {
+    public String tryMe() {
+        return "hello world";
+    }
+}
---
+++ src/test/java/com/gitinspector/MyTest.java
@@ -0,0 +1,18 @@
+/* Copyright Homeaway, Inc 2005-2008. All Rights Reserved.
+ * No unauthorized use of this software.
+ */
+package com.gitinspector;
+
+import org.junit.Test;
+
+/**
+ * bcorbett has not yet bothered to enter a useful javadoc comment.
+ */
+public class MyTest {
+
+    @Test
+    public void test() {
+        MyClass myClass = new MyClass();
+        System.out.println("myClass.tryMe() = " + myClass.tryMe());
+    }
+}
